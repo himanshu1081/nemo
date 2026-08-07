8092ee424bcc20fd785799352231aca4a52eb35a
himanshu1081 <himanshu1081c@gmail.com>
2026-08-08 03:20:22 +0530

    Add Alexa model and enhance endpoint functionality for user intents

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Alexa(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 @app.get("/")
 def home():
@@ -10,18 +17,33 @@ def home():
 
 
 @app.post("/alexa")
-def alexa():
+def alexa(request:Request):
     print("Server hit on alexa skill");
-    return {
-        "version": "1.0",
-        "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": "Hello Vexa this side"
-            },
-            "shouldEndSession": False
+    body = request.json()
+    userIntent = body.request.type
+    if(userIntent == "LaunchRequest"):
+        return {
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": "Hello Vexa this side"
+                },
+                "shouldEndSession": False
+            }
         }
-    }
+
+    if(userIntent=="ChatIntent"):
+        return {
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": "Hello let's talk"
+                },
+                "shouldEndSession": False
+            }
+        }
 
 
 @app.get("/cronjob")
